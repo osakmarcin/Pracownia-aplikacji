7d946b9680c582f5e2a437648be5b517b4516777
osakmarcin <58093184+osakmarcin@users.noreply.github.com>
2020-01-10 19:47:36 +0100

    Add files via upload

--- delete_user.php
+++ delete_user.php
@@ -1,13 +1,13 @@
 <?php
-
+session_start();
+require_once('./connect/connect.php')
 $connect=mysqli_connect("localhost","root","","komis");
-$id=$_GET[`id`];
+$id=$_GET['id'];
 
 $sql = "DELETE FROM `user` WHERE `user`.`id` = '$id'";
 
 if($result=mysqli_query($connect, $sql)){
-  echo"Usunięto użytkownika";
-}else{
-  echo"Usunieto użytkownika;
+$_SESSION['result']="Użytkownik został usunięty";
+  header('location:./index.php');
 }
  ?>
